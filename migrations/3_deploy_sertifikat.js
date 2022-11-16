f750cc8cdca3146e4b9819933dc583998cd01f09
Mirza <zanuarmirza@gmail.com>
2022-11-16 18:58:45 +0700

    fix: add .example

--- migrations/3_deploy_sertifikat.js
+++ migrations/3_deploy_sertifikat.js
@@ -1,7 +1,7 @@
 const Sertifikat = artifacts.require("Sertifikat");
 
 module.exports = function(deployer) {
-  const baseURI= ''
+  const baseURI= process.env.BASE_URI
   const name='Sertifikat-A'
   const symbol='STK-A'
   deployer.deploy(Sertifikat,baseURI,name,symbol);
